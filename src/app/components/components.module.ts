23a34667d035e4ca8e3393e326753e409d432d00
duoc <git.duoc@duoc.cl>
2023-08-28 14:12:31 -0400

    agregar footer

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EncabezadoComponent } from './encabezado/encabezado.component';
 import { IonicModule } from '@ionic/angular';
+import { FooterComponent } from './footer/footer.component';
 
 
 
 @NgModule({
-  declarations: [EncabezadoComponent],
-  exports:[EncabezadoComponent],
+  declarations: [EncabezadoComponent, FooterComponent],
+  exports:[EncabezadoComponent, FooterComponent],
   imports: [
     CommonModule,
     IonicModule,
